f34be84fa07352be27386bdb1e259ed9c2a55544
badhodigital <badhodigital@gmail.com>
2021-11-25 08:52:57 +0000

    new project

---
+++ src/components/header/header.data.js
@@ -0,0 +1,24 @@
+import lock from 'assets/images/icons/lock.png';
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  
+  {
+    path: 'features',
+    label: 'Services',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'testimonials',
+    label: 'Testimonials',
+  },
+  {
+    path:'why-us',
+    label:'About us',
+  },
+];
---
+++ src/pages/explore.js
@@ -0,0 +1,10 @@
+const Newpage = () => {
+    return ( 
+        <div>
+            <h1>Support</h1>
+            <p1>badhodigital cyberkalki</p1>
+        </div>
+     );
+}
+ 
+export default Newpage;
